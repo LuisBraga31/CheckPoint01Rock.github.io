69c4cfc0fbc135ffa7c7ddafa49b196fd1c6804b
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-03-13 15:08:11 -0300

    Novos ajustes e iniciando a responsavidade

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-const containerRef = document.querySelector('#containerPrincipal');
+const containerRef = document.querySelector('.containerCards');
 
-
-const userTituloRef = document.querySelector('#titulo');
-const userUrlRef = document.querySelector('#url');
-const userDescricaoRef = document.querySelector('#descricao');
-
-const cadastroButtonRef = document.querySelector('#enviar');
+const userTituloRef = document.querySelector('#tituloCard');
+const userUrlRef = document.querySelector('#urlCard');
+const userDescricaoRef = document.querySelector('#descricaoCard');
+const cadastroButtonRef = document.querySelector('#cadastroButton');
 
 var userData = {
 
@@ -27,20 +25,19 @@ function validarDescricao (descricao) {
     userData.descricao = descricao;
 }
 
-function cadastrar(event) {
+function cadastrarCard(event) {
 
     event.preventDefault();
 
     containerRef.innerHTML += `
     
     <div class="card">
-        <img src="${userData.url}" alt="" srcset=""> 
         <h3> ${userData.titulo} </h3>
+        <img src="${userData.url}" alt="" srcset=""> 
         <p> ${userData.descricao} </p>
     </div>
     
     `
-
     
 }
 
@@ -48,5 +45,4 @@ userTituloRef.addEventListener('keyup', (event) => validarTitulo(event.target.va
 userUrlRef.addEventListener('keyup', (event) => validarUrl(event.target.value));
 userDescricaoRef.addEventListener('keyup', (event) => validarDescricao(event.target.value));
 
-
-cadastroButtonRef.addEventListener('click', (event) => cadastrar(event));
+cadastroButtonRef.addEventListener('click', (event) => cadastrarCard(event));
